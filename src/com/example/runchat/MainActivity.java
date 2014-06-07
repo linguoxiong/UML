8c7a6e1c74066b22a9771617a4386297a3529eb9
leader <1019549951@qq.com>
2014-06-07 21:40:25 +0800

    test

---
+++ src/com/example/runchat/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.runchat;
+
+import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+//import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button sportButton = (Button)findViewById(R.id.sportButton);
+        final Button planButton = (Button)findViewById(R.id.planButton); 
+        
+        
+        sportButton.setOnClickListener(new OnClickListener () {
+        	public void onClick(View v) {
+        		Intent intent = new Intent();
+        		intent.setClass(MainActivity.this, RunningActivity.class); 	
+        		startActivity(intent);
+        		finish();
+        	}
+        });
+        
+        planButton.setOnClickListener(new OnClickListener() { 
+        	public void onClick(View v) {  
+        		
+        	} 
+        }); 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
---
+++ src/com/example/runchat/RunningActivity.java
@@ -0,0 +1,356 @@
+package com.example.runchat;
+
+//import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RunningActivity extends Activity {
+	private static final String TAG = "Pedometer";
+    private SharedPreferences mSettings;
+    private PedometerSettings mPedometerSettings;
+    private Utils mUtils;
+    
+    private TextView mStepValueView;
+    private TextView mPaceValueView;
+    private TextView mDistanceValueView;
+    private TextView mSpeedValueView;
+    private TextView mCaloriesValueView;
+    //TextView mDesiredPaceView;
+    private int mStepValue;//mStepValueView的值
+    private int mPaceValue;//mPaceValueView的值
+    private float mDistanceValue;//mDistanceValueView的值
+    private float mSpeedValue;//mSpeedValueView的值
+    private int mCaloriesValue;//mCaloriesValueView的值
+    private float mDesiredPaceOrSpeed;//
+    private int mMaintain;//是否是爬山
+    private boolean mIsMetric;//公制和米制切换标志
+    private float mMaintainInc;//
+    private boolean mQuitting = false; //
+    boolean pauseButtonState = false;
+    // Set when user selected Quit from menu, can be used by onPause, onStop, onDestroy
+
+    
+    /**
+     * True, when service is running.
+     */
+    private boolean mIsRunning;//程序是否运行的标志位
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "[ACTIVITY] onCreate");
+        super.onCreate(savedInstanceState);
+        
+        mStepValue = 0;
+        mPaceValue = 0;
+        
+        setContentView(R.layout.running);
+        
+        mUtils = Utils.getInstance();
+        
+    }
+    
+    //开始函数，重写该函数，加入日志。
+    @Override
+    protected void onStart() {
+        Log.i(TAG, "[ACTIVITY] onStart");
+        super.onStart();
+        //startStepService();
+        //bindStepService();
+    }
+
+    //重写回复函数
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "[ACTIVITY] onResume");
+        super.onResume();
+        
+        mSettings = PreferenceManager.getDefaultSharedPreferences(this);
+        mPedometerSettings = new PedometerSettings(mSettings);
+        
+        
+        // Read from preferences if the service was running on the last onPause
+        mIsRunning = mPedometerSettings.isServiceRunning();
+        
+        // Start the service if this is considered to be an application start (last onPause was long ago)
+        if (!mIsRunning && mPedometerSettings.isNewStart()) {
+            startStepService();
+            bindStepService();
+        }
+        else if (mIsRunning) {
+            bindStepService();
+        }
+        
+        mPedometerSettings.clearServiceRunning();
+
+        //mStepValueView     = (TextView) findViewById(R.id.step_value);
+        //mPaceValueView     = (TextView) findViewById(R.id.pace_value);
+        mDistanceValueView = (TextView) findViewById(R.id.distanceText);
+        mSpeedValueView    = (TextView) findViewById(R.id.averageSpeed);
+        mCaloriesValueView = (TextView) findViewById(R.id.calorieRunningText);
+
+        mIsMetric = mPedometerSettings.isMetric();     
+        mMaintain = mPedometerSettings.getMaintainOption();
+        if (mMaintain == PedometerSettings.M_PACE) {
+            mMaintainInc = 5f;
+            mDesiredPaceOrSpeed = (float)mPedometerSettings.getDesiredPace();
+        }
+        else 
+        if (mMaintain == PedometerSettings.M_SPEED) {
+            mDesiredPaceOrSpeed = mPedometerSettings.getDesiredSpeed();
+            mMaintainInc = 0.1f;
+        }
+        Button finishButton = (Button) findViewById(R.id.finish);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	resetValues(false);
+                unbindStepService();
+                stopStepService();
+            }
+        });
+        
+        Button pauseButton = (Button) findViewById(R.id.pause);
+        pauseButton.setOnClickListener(new OnClickListener(){
+     	   public void onClick(View v) {
+    		   if(!pauseButtonState) {
+    			   unbindStepService();
+    			   stopStepService();
+    			   pauseButtonState = true;
+    		   }
+    		   else {
+    			   startStepService();
+                   bindStepService();
+                   pauseButtonState = true;
+    		   }
+    			   
+    	   }
+        });
+        //displayDesiredPaceOrSpeed();
+    }
+    
+    /*
+    private void displayDesiredPaceOrSpeed() {
+        if (mMaintain == PedometerSettings.M_PACE) {
+            mDesiredPaceView.setText("" + (int)mDesiredPaceOrSpeed);
+        }
+        else {
+            mDesiredPaceView.setText("" + mDesiredPaceOrSpeed);
+        }
+    }*/
+    
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "[ACTIVITY] onPause");
+        if (mIsRunning) {
+            unbindStepService();
+        }
+        if (mQuitting) {
+            mPedometerSettings.saveServiceRunningWithNullTimestamp(mIsRunning);
+        }
+        else {
+            mPedometerSettings.saveServiceRunningWithTimestamp(mIsRunning);
+        }
+
+        super.onPause();
+        savePaceSetting();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "[ACTIVITY] onStop");
+        super.onStop();
+    }
+
+    protected void onDestroy() {
+        Log.i(TAG, "[ACTIVITY] onDestroy");
+        super.onDestroy();
+    }
+    
+    protected void onRestart() {
+        Log.i(TAG, "[ACTIVITY] onRestart");
+        super.onDestroy();
+    }
+/*
+    private void setDesiredPaceOrSpeed(float desiredPaceOrSpeed) {
+        if (mService != null) {
+            if (mMaintain == PedometerSettings.M_PACE) {
+                mService.setDesiredPace((int)desiredPaceOrSpeed);
+            }
+            else
+            if (mMaintain == PedometerSettings.M_SPEED) {
+                mService.setDesiredSpeed(desiredPaceOrSpeed);
+            }
+        }
+    }
+ */   
+    private void savePaceSetting() {
+        mPedometerSettings.savePaceOrSpeedSetting(mMaintain, mDesiredPaceOrSpeed);
+    }
+
+    private StepService mService;
+    
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = ((StepService.StepBinder)service).getService();
+
+            mService.registerCallback(mCallback);
+            mService.reloadSettings();  
+            
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            mService = null;
+        }
+    };
+    
+
+    private void startStepService() {
+        if (! mIsRunning) {
+            Log.i(TAG, "[SERVICE] Start");
+            mIsRunning = true;
+            startService(new Intent(RunningActivity.this,
+                    StepService.class));
+        }
+    }
+    
+    private void bindStepService() {
+        Log.i(TAG, "[SERVICE] Bind");
+        bindService(new Intent(RunningActivity.this, 
+                StepService.class), mConnection, Context.BIND_AUTO_CREATE + Context.BIND_DEBUG_UNBIND);
+    }
+
+    private void unbindStepService() {
+        Log.i(TAG, "[SERVICE] Unbind");
+        unbindService(mConnection);
+    }
+    
+    private void stopStepService() {
+        Log.i(TAG, "[SERVICE] Stop");
+        if (mService != null) {
+            Log.i(TAG, "[SERVICE] stopService");
+            stopService(new Intent(RunningActivity.this,
+                  StepService.class));
+        }
+        mIsRunning = false;
+    }
+    
+  
+    private void resetValues(boolean updateDisplay) {
+        if (mService != null && mIsRunning) {
+            mService.resetValues();                    
+        }
+        else {
+            //mStepValueView.setText("0");
+            //mPaceValueView.setText("0");
+            mDistanceValueView.setText("0");
+            mSpeedValueView.setText("0");
+            mCaloriesValueView.setText("0");
+            SharedPreferences state = getSharedPreferences("state", 0);
+            SharedPreferences.Editor stateEditor = state.edit();
+            if (updateDisplay) {
+                stateEditor.putInt("steps", 0);
+                stateEditor.putInt("pace", 0);
+                stateEditor.putFloat("distance", 0);
+                stateEditor.putFloat("speed", 0);
+                stateEditor.putFloat("calories", 0);
+                stateEditor.commit();
+            }
+        }
+    }
+    
+    //       
+    private StepService.ICallback mCallback = new StepService.ICallback() {
+        public void stepsChanged(int value) {
+            mHandler.sendMessage(mHandler.obtainMessage(STEPS_MSG, value, 0));
+        }
+        public void paceChanged(int value) {
+            mHandler.sendMessage(mHandler.obtainMessage(PACE_MSG, value, 0));
+        }
+        public void distanceChanged(float value) {
+            mHandler.sendMessage(mHandler.obtainMessage(DISTANCE_MSG, (int)(value*1000), 0));
+        }
+        public void speedChanged(float value) {
+            mHandler.sendMessage(mHandler.obtainMessage(SPEED_MSG, (int)(value*1000), 0));
+        }
+        public void caloriesChanged(float value) {
+            mHandler.sendMessage(mHandler.obtainMessage(CALORIES_MSG, (int)(value), 0));
+        }
+    };
+    
+    private static final int STEPS_MSG = 1;
+    private static final int PACE_MSG = 2;
+    private static final int DISTANCE_MSG = 3;
+    private static final int SPEED_MSG = 4;
+    private static final int CALORIES_MSG = 5;
+    
+    @SuppressLint("HandlerLeak") private Handler mHandler = new Handler() {
+        @Override public void handleMessage(Message msg) {
+            switch (msg.what) {
+            /*
+                case STEPS_MSG:
+                    mStepValue = (int)msg.arg1;
+                    mStepValueView.setText("" + mStepValue);
+                    break;
+                case PACE_MSG:
+                    mPaceValue = msg.arg1;
+                    if (mPaceValue <= 0) { 
+                        mPaceValueView.setText("0");
+                    }
+                    else {
+                        mPaceValueView.setText("" + (int)mPaceValue);
+                    }
+                    break;
+              */
+                case DISTANCE_MSG:
+                    mDistanceValue = ((int)msg.arg1)/1000f;
+                    if (mDistanceValue <= 0) { 
+                        mDistanceValueView.setText("0");
+                    }
+                    else {
+                        mDistanceValueView.setText(
+                                ("" + (mDistanceValue + 0.000001f)).substring(0, 5)
+                        );
+                    }
+                    break;
+                case SPEED_MSG:
+                    mSpeedValue = ((int)msg.arg1)/1000f;
+                    if (mSpeedValue <= 0) { 
+                        mSpeedValueView.setText("0");
+                    }
+                    else {
+                        mSpeedValueView.setText(
+                                ("" + (mSpeedValue + 0.000001f)).substring(0, 4)
+                        );
+                    }
+                    break;
+                case CALORIES_MSG:
+                    mCaloriesValue = msg.arg1;
+                    if (mCaloriesValue <= 0) { 
+                        mCaloriesValueView.setText("0");
+                    }
+                    else {
+                        mCaloriesValueView.setText("" + (int)mCaloriesValue);
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+        
+    };
+}
